847aa3704363b76b6ac4c031af2e825e04f81f3e
Roxanne Panthaky <rpanthak@ciena.com>
2016-06-17 16:51:20 -0400

    fixed ps destroy issues.

--- addon/components/modal-perfect-scroll.js
+++ addon/components/modal-perfect-scroll.js
@@ -6,10 +6,10 @@ const {
 import PerfectScroll from 'ember-perfect-scroll/components/perfect-scroll/component'
 
 export default PerfectScroll.extend({
-  _destroyPerfectScroll: on('willDestroyElement', function () {
+  willDestroyElement () {
     var el = document.getElementById(get(this, 'eId'))
     if (el) {
       window.Ps.destroy(el)
     }
-  })
+  }
 })

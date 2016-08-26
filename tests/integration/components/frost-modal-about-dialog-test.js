63528e75400d1e53e7db001f81f917053085cda4
Eric White <ericwhite613@gmail.com>
2016-08-26 13:58:37 -0400

    Add in more tests

--- tests/integration/components/frost-modal-about-dialog-test.js
+++ tests/integration/components/frost-modal-about-dialog-test.js
@@ -1,6 +1,8 @@
 /* jshint expr:true */
 /* global capture */
-import { expect } from 'chai'
+import {
+  expect
+} from 'chai'
 import {
   $hook,
   initialize
@@ -9,15 +11,18 @@ import {
   describeComponent,
   it
 } from 'ember-mocha'
-import {initialize as initializeSvgUse} from 'ember-frost-core/instance-initializers/svg-use-polyfill'
+import {
+  initialize as initializeSvgUse
+} from 'ember-frost-core/instance-initializers/svg-use-polyfill'
 
 import hbs from 'htmlbars-inline-precompile'
-import { beforeEach } from 'mocha'
+import {
+  beforeEach
+} from 'mocha'
 import Ember from 'ember'
 describeComponent(
   'frost-modal-about-dialog',
-  'Integration: FrostModalAboutDialogComponent',
-  {
+  'Integration: FrostModalAboutDialogComponent', {
     integration: true
   },
   function () {
@@ -34,7 +39,7 @@ describeComponent(
       })
       this.set('isModalVisible', true)
 
-      this.render(hbs`
+      this.render(hbs `
         {{frost-modal-outlet}}
 
         {{frost-modal-about
@@ -59,11 +64,148 @@ describeComponent(
           onClose=(action closeModal)
         }}
       `)
-      // eslint-disable-next-line
+        // eslint-disable-next-line
       expect($hook('about-dialog-modal'), 'Is modal visible')
         .to.have.length(1)
       Ember.run.later(function () {
-        return capture('about', {targetElement: this.$('.frost-modal-outlet-container.about')[0], experimentalSvgs: true}).then(function () {
+        return capture('about', {
+          targetElement: this.$('.frost-modal-outlet-container.about')[0],
+          experimentalSvgs: true
+        }).then(function () {
+          done()
+        }).catch(function (err) {
+          done(err)
+        })
+      }, 2000)
+    })
+
+    it('renders with product', function (done) {
+      this.timeout(10000)
+      this.set('closeModal', () => {
+        this.set('isModalVisible', false)
+      })
+      this.set('isModalVisible', true)
+
+      this.render(hbs `
+        {{frost-modal-outlet}}
+
+        {{frost-modal-about
+          brandingStrip=(hash
+            icon='branding-strip'
+            pack='dummy'
+          )
+          copyright='copyright'
+          hook='about-dialog'
+          isVisible=isModalVisible
+          logo=(hash
+            icon='logo'
+            pack='dummy'
+          )
+          product=(hash
+            icon='product'
+            pack='dummy'
+          )
+          onClose=(action closeModal)
+        }}
+      `)
+        // eslint-disable-next-line
+      expect($hook('about-dialog-modal'), 'Is modal visible')
+        .to.have.length(1)
+      Ember.run.later(function () {
+        return capture('about-with-product', {
+          targetElement: this.$('.frost-modal-outlet-container.about')[0],
+          experimentalSvgs: true
+        }).then(function () {
+          done()
+        }).catch(function (err) {
+          done(err)
+        })
+      }, 2000)
+    })
+
+    it('renders about-with-multiple-verions', function (done) {
+      this.timeout(10000)
+      this.set('closeModal', () => {
+        this.set('isModalVisible', false)
+      })
+      this.set('isModalVisible', true)
+
+      this.render(hbs `
+        {{frost-modal-outlet}}
+
+        {{frost-modal-about
+          brandingStrip=(hash
+            icon='branding-strip'
+            pack='dummy'
+          )
+          copyright='copyright'
+          hook='about-dialog'
+          isVisible=isModalVisible
+          logo=(hash
+            icon='logo'
+            pack='dummy'
+          )
+          versions=(array
+            'Version: 1.0.0' 'Version: 2.3.4'
+          )
+          onClose=(action closeModal)
+        }}
+      `)
+        // eslint-disable-next-line
+      expect($hook('about-dialog-modal'), 'Is modal visible')
+        .to.have.length(1)
+      Ember.run.later(function () {
+        return capture('about-with-multiple-verions', {
+          targetElement: this.$('.frost-modal-outlet-container.about')[0],
+          experimentalSvgs: true
+        }).then(function () {
+          done()
+        }).catch(function (err) {
+          done(err)
+        })
+      }, 2000)
+    })
+
+    it('renders about-with-multiple-verions', function (done) {
+      this.timeout(10000)
+      this.set('closeModal', () => {
+        this.set('isModalVisible', false)
+      })
+      this.set('isModalVisible', true)
+
+      this.render(hbs `
+        {{frost-modal-outlet}}
+
+        {{frost-modal-about
+          brandingStrip=(hash
+            icon='branding-strip'
+            pack='dummy'
+          )
+          copyright='copyright'
+          hook='about-dialog'
+          isVisible=isModalVisible
+          logo=(hash
+            icon='logo'
+            pack='dummy'
+          )
+          product=(hash
+            icon='product'
+            pack='dummy'
+          )
+          versions=(array
+            'Version: 1.0.0' 'Version: 2.3.4'
+          )
+          onClose=(action closeModal)
+        }}
+      `)
+        // eslint-disable-next-line
+      expect($hook('about-dialog-modal'), 'Is modal visible')
+        .to.have.length(1)
+      Ember.run.later(function () {
+        return capture('about-with-product-and-multiple-verions', {
+          targetElement: this.$('.frost-modal-outlet-container.about')[0],
+          experimentalSvgs: true
+        }).then(function () {
           done()
         }).catch(function (err) {
           done(err)
@@ -73,15 +215,15 @@ describeComponent(
   }
 )
 
-  // capture('about')
-  // capture('about-with-product')
-  // capture('about-with-multiple-verions')
-  // capture('about-with-product-and-multiple-verions')
+// capture('about')
+// capture('about-with-product')
+// capture('about-with-multiple-verions')
+// capture('about-with-product-and-multiple-verions')
 
-  // data-test={{hook (concat hook '-close')}}
-  // onclick={{action (action onClose)}}
-  // hook=(concat hook '-branding-strip')
-  // hook=(concat hook '-logo')
-  // hook=(concat hook '-product')
-  // data-test={{hook (concat hook '-version') index=index}}
-  // data-test={{hook (concat hook '-copyright')}}
+// data-test={{hook (concat hook '-close')}}
+// onclick={{action (action onClose)}}
+// hook=(concat hook '-branding-strip')
+// hook=(concat hook '-logo')
+// hook=(concat hook '-product')
+// data-test={{hook (concat hook '-version') index=index}}
+// data-test={{hook (concat hook '-copyright')}}

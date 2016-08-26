524675ebf3dcedcf9ba393fabb5829b4eeb901f0
Steven Glanzer <sglanzer@ciena.com>
2016-08-25 22:18:41 -0400

    Adding missing tests, removing orphaned tests

---
+++ tests/unit/helpers/frost-modal-outlet-animation-test.js
@@ -0,0 +1,17 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describe,
+  it
+} from 'mocha';
+import {
+  frostModalOutletAnimation
+} from 'ember-frost-modal/helpers/frost-modal-outlet-animation';
+
+describe('FrostModalOutletAnimationHelper', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    let result = frostModalOutletAnimation(42);
+    expect(result).to.be.ok;
+  });
+});

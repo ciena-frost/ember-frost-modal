649f986e96ab512781b2a7ee07aece37ed6c5921
Matthew Dahl <dahl.matthew@gmail.com>
2016-10-11 08:49:39 -0700

    accept more patches

--- blueprints/ember-frost-modal/index.js
+++ blueprints/ember-frost-modal/index.js
@@ -5,11 +5,11 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-elsewhere', target: '0.4.1'},
+        {name: 'ember-elsewhere', target: '~0.4.1'},
         {name: 'ember-frost-core', target: '>=0.23.0 <1.0.0'},
-        {name: 'ember-get-config', target: '0.1.11'},
+        {name: 'ember-get-config', target: '~0.1.11'},
         {name: 'ember-ignore-children-helper', target: '^1.0.0'},
-        {name: 'liquid-fire', target: '0.25.0'}
+        {name: 'liquid-fire', target: '~0.25.0'}
       ]
     })
   }

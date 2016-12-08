04a9e96482ab5d5a503476513f081a921a74a8b0
nicklewanowicz <nicklewanowicz@gmail.com>
2016-12-08 15:34:57 -0500

    updated mocha

--- blueprints/ember-frost-modal/index.js
+++ blueprints/ember-frost-modal/index.js
@@ -6,7 +6,7 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         {name: 'ember-elsewhere', target: '~0.4.1'},
-        {name: 'ember-frost-core', target: '^1.0.3'},
+        {name: 'ember-frost-core', target: '^1.3.0'},
         {name: 'ember-get-config', target: '~0.1.11'},
         {name: 'ember-ignore-children-helper', target: '^1.0.0'},
         {name: 'liquid-fire', target: '~0.26.1'}
